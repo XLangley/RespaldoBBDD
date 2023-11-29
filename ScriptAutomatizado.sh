17e1699c5f9f28db32d92283210fa838f4468586
xLangley <alfr.galdames@duocuc.cl>
2023-11-29 01:14:56 +0000

    Respaldo 11

--- ScriptAutomatizado.sh
+++ ScriptAutomatizado.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-# Cambiar al directorio del repositorio Git
 cd /home/seguridad/RespaldoBBDD/
 
-# Obtener el último mensaje de commit
 last_commit_message=$(git log -1 --pretty=%B)
 
-# Extraer el número del respaldo del mensaje del commit anterior
 last_backup_number=$(echo "$last_commit_message" | grep -oP '\bRespaldo \K\d+' || echo 0)
 
-# Incrementar el número del respaldo para el nuevo commit
 new_backup_number=$((last_backup_number + 1))
 
-# Respaldar la base de datos MySQL
 mysqldump -u seguridad -pseguridad --databases Evaluacion4 > /home/seguridad/RespaldoBBDD/Respaldo${new_backup_number}.sql
 
-# Agregar los cambios al repositorio Git
+max_respaldos=1
+num_respaldos=$(ls -1 Respaldo*.sql | wc -l)
+if [ "$num_respaldos" -gt "$max_respaldos" ]; then
+    cantidad_a_eliminar=$((num_respaldos - max_respaldos))
+    ls -1t Respaldo*.sql | tail -n "$cantidad_a_eliminar" | xargs rm
+fi
+
 git add .
 
-# Realizar un commit con un mensaje incremental
 git commit -m "Respaldo ${new_backup_number}"
 
-# Hacer un push al repositorio remoto (cambiar "main" por el nombre de tu rama si es diferente)
 git push origin main
 
